b7a9df8b0ff948198f8b84b7986d031a19deae84
veroinesc <veroinesc@yahoo.com.ar>
2024-06-01 16:59:00 -0300

    Update hope.py
    solved k error

--- src/bionev/OpenNE/hope.py
+++ src/bionev/OpenNE/hope.py
@@ -1,12 +1,9 @@
-# -*- coding: utf-8 -*-
-
 import networkx as nx
 import numpy as np
 import scipy.sparse.linalg as lg
 
 __author__ = "Alan WANG"
 __email__ = "alan1995wang@outlook.com"
-
 
 class HOPE(object):
     def __init__(self, graph, d):
@@ -20,25 +17,23 @@ class HOPE(object):
         self.learn_embedding()
 
     def learn_embedding(self):
-
         graph = self.g.G
-        A = nx.to_numpy_matrix(graph)
-
-        # self._beta = 0.0728
-
-        # M_g = np.eye(graph.number_of_nodes()) - self._beta * A
-        # M_l = self._beta * A
+        A = nx.to_numpy_array(graph)
 
         M_g = np.eye(graph.number_of_nodes())
         M_l = np.dot(A, A)
 
         S = np.dot(np.linalg.inv(M_g), M_l)
-        # s: \sigma_k
-        u, s, vt = lg.svds(S, k=self._d // 2)
+
+        # Ajustar el valor de k para que esté dentro del rango permitido
+        k = min(self._d // 2, min(S.shape) - 1)
+        if k <= 0:
+            raise ValueError("El valor ajustado de 'k' no es válido. Verifique el tamaño del grafo y la dimensión deseada.")
+
+        u, s, vt = lg.svds(S, k=k)
         sigma = np.diagflat(np.sqrt(s))
         X1 = np.dot(u, sigma)
         X2 = np.dot(vt.T, sigma)
-        # self._X = X2
         self._X = np.concatenate((X1, X2), axis=1)
 
     @property
@@ -50,10 +45,8 @@ class HOPE(object):
         return vectors
 
     def save_embeddings(self, filename):
-        fout = open(filename, 'w')
-        node_num = len(self.vectors.keys())
-        fout.write("{} {}\n".format(node_num, self._d))
-        for node, vec in self.vectors.items():
-            fout.write("{} {}\n".format(node,
-                                        ' '.join([str(x) for x in vec])))
-        fout.close()
+        with open(filename, 'w') as fout:
+            node_num = len(self.vectors.keys())
+            fout.write("{} {}\n".format(node_num, self._d))
+            for node, vec in self.vectors.items():
+                fout.write("{} {}\n".format(node, ' '.join([str(x) for x in vec])))
