b5db889477b6fefee01fca972e2ab526bd333656
veroinesc <veroinesc@yahoo.com.ar>
2024-06-01 16:14:29 -0300

    Update line.py
    version funcional

--- src/bionev/OpenNE/line.py
+++ src/bionev/OpenNE/line.py
@@ -5,9 +5,12 @@ import random
 
 import numpy as np
 import tensorflow as tf
+tf.compat.v1.disable_eager_execution()
 from sklearn.linear_model import LogisticRegression
 
 from bionev.OpenNE.classify import Classifier, read_node_label
+
+
 
 
 class _LINE(object):
@@ -22,44 +25,41 @@ class _LINE(object):
         self.negative_ratio = negative_ratio
 
         self.gen_sampling_table()
-        self.sess = tf.Session()
+        self.sess = tf.compat.v1.Session()
         cur_seed = random.getrandbits(32)
-        initializer = tf.contrib.layers.xavier_initializer(
-            uniform=False, seed=cur_seed)
-        with tf.variable_scope("model", reuse=None, initializer=initializer):
+        initializer = tf.keras.initializers.GlorotUniform(seed=cur_seed)
+        with tf.compat.v1.variable_scope("model", reuse=None, initializer=initializer):
             self.build_graph()
-        self.sess.run(tf.global_variables_initializer())
-
-    def build_graph(self):
-        self.h = tf.placeholder(tf.int32, [None])
-        self.t = tf.placeholder(tf.int32, [None])
-        self.sign = tf.placeholder(tf.float32, [None])
+        self.sess.run(tf.compat.v1.global_variables_initializer())
+        
+    def build_graph(self):		 
+        self.h = tf.compat.v1.placeholder(tf.int32, [None])
+        self.t = tf.compat.v1.placeholder(tf.int32, [None])
+        self.sign = tf.compat.v1.placeholder(tf.float32, [None])
 
         cur_seed = random.getrandbits(32)
-        self.embeddings = tf.get_variable(name="embeddings" + str(self.order), shape=[
-            self.node_size, self.rep_size], initializer=tf.contrib.layers.xavier_initializer(uniform=False,
-                                                                                             seed=cur_seed))
-        self.context_embeddings = tf.get_variable(name="context_embeddings" + str(self.order), shape=[
-            self.node_size, self.rep_size], initializer=tf.contrib.layers.xavier_initializer(uniform=False,
-                                                                                             seed=cur_seed))
-        # self.h_e = tf.nn.l2_normalize(tf.nn.embedding_lookup(self.embeddings, self.h), 1)
-        # self.t_e = tf.nn.l2_normalize(tf.nn.embedding_lookup(self.embeddings, self.t), 1)
-        # self.t_e_context = tf.nn.l2_normalize(tf.nn.embedding_lookup(self.context_embeddings, self.t), 1)
+        initializer = tf.keras.initializers.GlorotUniform(seed=cur_seed)
+
+        self.embeddings = tf.Variable(initial_value=initializer((self.node_size, self.rep_size)), 
+                                      name="embeddings" + str(self.order))
+        self.context_embeddings = tf.Variable(initial_value=initializer((self.node_size, self.rep_size)), 
+                                              name="context_embeddings" + str(self.order))
+
         self.h_e = tf.nn.embedding_lookup(self.embeddings, self.h)
         self.t_e = tf.nn.embedding_lookup(self.embeddings, self.t)
-        self.t_e_context = tf.nn.embedding_lookup(
-            self.context_embeddings, self.t)
-        self.second_loss = -tf.reduce_mean(tf.log_sigmoid(
+        self.t_e_context = tf.nn.embedding_lookup(self.context_embeddings, self.t)
+        self.second_loss = -tf.reduce_mean(tf.math.log_sigmoid(
             self.sign * tf.reduce_sum(tf.multiply(self.h_e, self.t_e_context), axis=1)))
-        self.first_loss = -tf.reduce_mean(tf.log_sigmoid(
+        self.first_loss = -tf.reduce_mean(tf.math.log_sigmoid(
             self.sign * tf.reduce_sum(tf.multiply(self.h_e, self.t_e), axis=1)))
         if self.order == 1:
             self.loss = self.first_loss
         else:
             self.loss = self.second_loss
-        optimizer = tf.train.AdamOptimizer(0.001)
+        optimizer = tf.compat.v1.train.AdamOptimizer(0.001)
         self.train_op = optimizer.minimize(self.loss)
-
+		
+		
     def train_one_epoch(self):
         sum_loss = 0.0
         batches = self.batch_iter()
