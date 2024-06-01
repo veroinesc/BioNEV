04df3a1d68fa9386d1783f56280aff4aebf96da4
veroinesc <veroinesc@yahoo.com.ar>
2024-06-01 18:06:03 -0300

    Update hope.py
    now must work with all matrix sizes

--- src/bionev/OpenNE/hope.py
+++ src/bionev/OpenNE/hope.py
@@ -1,52 +1,47 @@
 import networkx as nx
 import numpy as np
-import scipy.sparse.linalg as lg
+from scipy.sparse.linalg import eigsh
+import scipy.sparse as sp
 
-__author__ = "Alan WANG"
-__email__ = "alan1995wang@outlook.com"
-
-class HOPE(object):
-    def __init__(self, graph, d):
-        '''
-          d: representation vector dimension
-        '''
-        self._d = d
-        self._graph = graph.G
+class LaplacianEigenmaps(object):
+    def __init__(self, graph, rep_size=128):
         self.g = graph
-        self._node_num = graph.node_size
-        self.learn_embedding()
+        self.node_size = self.g.number_of_nodes()
+        self.rep_size = rep_size
+        self.adj_mat = nx.to_scipy_sparse_matrix(self.g)
+        self.vectors = {}
+        self.embeddings = self.get_train()
+        look_back = list(self.g.nodes())
 
-    def learn_embedding(self):
-        graph = self.g.G
-        A = nx.to_numpy_array(graph)
+        for i, embedding in enumerate(self.embeddings):
+            self.vectors[look_back[i]] = embedding
 
-        M_g = np.eye(graph.number_of_nodes())
-        M_l = np.dot(A, A)
+    def getLap(self):
+        G = self.g.to_undirected()
+        print('begin norm_lap_mat')
+        norm_lap_mat = nx.normalized_laplacian_matrix(G)
+        print('finish norm_lap_mat')
+        return norm_lap_mat
 
-        S = np.dot(np.linalg.inv(M_g), M_l)
-
-        # Ajustar el valor de k para que esté dentro del rango permitido
-        k = min(self._d // 2, min(S.shape) - 1)
-        if k <= 0:
-            raise ValueError("El valor ajustado de 'k' no es válido. Verifique el tamaño del grafo y la dimensión deseada.")
-
-        u, s, vt = lg.svds(S, k=k)
-        sigma = np.diagflat(np.sqrt(s))
-        X1 = np.dot(u, sigma)
-        X2 = np.dot(vt.T, sigma)
-        self._X = np.concatenate((X1, X2), axis=1)
-
-    @property
-    def vectors(self):
-        vectors = {}
-        look_back = self.g.look_back_list
-        for i, embedding in enumerate(self._X):
-            vectors[look_back[i]] = embedding
-        return vectors
+    def get_train(self):
+        lap_mat = self.getLap()
+        print('finish getLap...')
+        w, vec = eigsh(lap_mat, k=self.rep_size)
+        print('finish eigen decomposition...')
+        return vec
 
     def save_embeddings(self, filename):
-        with open(filename, 'w') as fout:
-            node_num = len(self.vectors.keys())
-            fout.write("{} {}\n".format(node_num, self._d))
-            for node, vec in self.vectors.items():
-                fout.write("{} {}\n".format(node, ' '.join([str(x) for x in vec])))
+        fout = open(filename, 'w')
+        node_num = len(self.vectors)
+        fout.write("{} {}\n".format(node_num, self.rep_size))
+        for node, vec in self.vectors.items():
+            fout.write("{} {}\n".format(node, ' '.join([str(x) for x in vec])))
+        fout.close()
+
+# Ejemplo de uso:
+# graph_small = nx.read_edgelist("small_graph.edgelist")
+# graph_large = nx.read_edgelist("large_graph.edgelist")
+# laplacian_small = LaplacianEigenmaps(graph_small)
+# laplacian_large = LaplacianEigenmaps(graph_large)
+# laplacian_small.save_embeddings("embeddings_small.txt")
+# laplacian_large.save_embeddings("embeddings_large.txt")
